c27f75940675fa4d3e691d0b93eccaa7f457f57c
Ricardo Almeida <ricardoluiz1.almeida@gmail.com>
2010-06-06 18:18:33 -0300

    haml --rails isnt necessary

--- main.rb
+++ main.rb
@@ -30,7 +30,6 @@ run "bundle install"
 generate "rspec:install"
 generate "cucumber:skeleton --capybara --rspec --spork"
 generate "simple_form:install"
-run "haml --rails ."
 run "gem install compass"
 run "compass init --using blueprint --app rails"
 
